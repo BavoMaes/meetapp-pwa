d3056d3e386a578b5bfcd4e82978587b4b1318b0
Bavo Maes <bavo.maes@telenet.be>
2020-05-29 17:04:13 +0200

    feat: Back button in top menu bar

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         <div class="topbar">
-            <div class="topbar__back" v-if="this.$route.name !== 'Info'"></div>
+            <div class="topbar__back" @click="$router.go(-1)" v-if="this.$route.name !== 'Info' && this.$route.name !== 'Discover' && this.$route.name !== 'Chat'">
+                <svg class="topbar__icon" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+                    <polyline points="244 400 100 256 244 112" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px" />
+                    <line x1="120" y1="256" x2="412" y2="256" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px" />
+                </svg>
+            </div>
             <div class="topbar__blank" v-else></div>
             <h1 class="topbar__title">{{ $route.name }}</h1>
             <div class="topbar__profile" v-if="this.$route.name !== 'Info' && this.$route.name !== 'My profile'">
@@ -35,6 +40,12 @@ export default {
     &__back {
         margin-left: 20px;
         width: 30px;
+    }
+
+    &__icon {
+        width: 30px;
+        height: 30px;
+        stroke: $navigation-active;
     }
 
     &__title {
