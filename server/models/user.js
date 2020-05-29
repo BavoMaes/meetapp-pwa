e22f678e547d3032f28e6cb930180ae0c7587931
Bavo Maes <bavo.maes@telenet.be>
2020-05-29 17:07:25 +0200

    feat: Add user to database

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+    {
+        firstname: { type: String, required: true },
+        lastname: { type: String, required: true },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+        }
+    },
+    { timestamps: true }
+);
+
+const userModel = mongoose.model('User', userSchema);
+const functions = require('./functions')(userModel);
+
+module.exports = {
+    model: userModel,
+    functions: functions
+}
