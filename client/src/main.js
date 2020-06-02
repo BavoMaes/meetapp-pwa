3e5e573cb9cb800fe5ca0b0c261c6ca5b9ecae4a
Bavo Maes <bavo.maes@telenet.be>
2020-06-02 15:51:40 +0200

    feat: Socket.io connection on client

--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
+import VueSocketIO from 'vue-socket.io';
+import './registerServiceWorker';
 import './assets/css/reset.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// vue-socket.io
+// https://www.npmjs.com/package/vue-socket.io
+// Laatst geraadpleegd op 2 juni 2020
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:5000',
+    vuex: {
+      store,
+      actionPrefix: 'SOCKET_',
+      mutationPrefix: 'SOCKET_'
+    }
+}));
 
 new Vue({
   router,
