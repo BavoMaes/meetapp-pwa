dcc7c10f171a67950264d8274f3a17fdb27773b2
Bavo Maes <bavo.maes@telenet.be>
2020-06-02 15:51:25 +0200

    feat: Socket.io listener on server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const database = require('./config/database');
 
-const app = express();
 const port = process.env.PORT || 5000;
 
-/* Initialize connections */
+/* Initialize database */
 
 database.init();
 
-/* Main logic */
-
-
-
 /* Start Express server */
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const app = express();
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
+
+/* Start Socket.io */
+
+const io = socket(server);
+io.on('connection', () => {
+  console.log('A user connected...');
+})
+
+
